c0b6f630b49dcf88768ca2bf515f5b092dc6f65d
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-12-14 11:24:47 +0100

    Putting jwt in place

--- 01-API-Rest/src/routes/login.routes.ts
+++ 01-API-Rest/src/routes/login.routes.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import Joi from 'joi'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 import { dataSource } from '../config/dataSource'
 import { User } from '../entity/user.entity'
@@ -28,15 +30,24 @@ router.post("/login", async (req: Request, res: Response) => {
 
     const user = await dataSource.getRepository(User).findOneBy({ name })
 
-    console.log(user)
-
     if (!user) {
-     res.status(401).json({
+     return res.status(401).json({
        message: "User not found"
      }) 
     } 
 
-    res.send("foo")
+    const arePasswordsMatching = await bcrypt.compare(password, user.password)
+
+    if (!arePasswordsMatching) {
+      return res.status(401).json({
+        message: "Invalid password"
+      })
+    }
+
+    const token = jwt.sign({ userId: user.id }, 'secret_key', { expiresIn: '1h' })
+
+
+    return res.json({token})
   }
 })
 
